757aac88d80ef8f26c63ffaa86276a04d7bb5d47
Abhinay Kumar <kabhinayak02@gmail.com>
2023-07-24 01:57:22 +0530

    Added express validator

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-// const mongoURI = "mongodb://localhost:27017/"
-const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1"
-
-const connectToMongo=()=>{
-    mongoose.connect(mongoURI);
-    console.log("connected to mongo");
+const mongoURI = "mongodb://localhost:27017"
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI)
+    console.log("Connected to Mongo Successfully");
 }
 
 module.exports = connectToMongo;
