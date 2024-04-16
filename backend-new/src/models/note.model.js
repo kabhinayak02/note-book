588fcdbcba7a43352b5384b32d23b70702aef959
Abhinay Kumar <kabhinayak02@gmail.com>
2024-04-16 18:44:12 +0530

    add models

---
+++ backend-new/src/models/note.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose';
+
+const noteSchema = new Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true, 
+    },
+    tag:{
+        type: String,
+        default: "General"
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+  });
+
+export const Note = mongoose.model("Note", noteSchema);
